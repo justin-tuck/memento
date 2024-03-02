835619277ed288b971d287dcf36d6046b739cd9a
justin-tuck <p.justin.tuck@gmail.com>
2024-03-02 09:52:31 -0500

    updated readme and made it look better on mobile

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 npm install -g firebase-tools
 firebase init --hosting
+
+## Run
+npm run start
+
+### Tips
+Toggle mobile device with `Ctrl+Shift+M` in dev tools in chrome
+
+## Build and deploy
 npm run build
 firebase deploy
--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ScoreBoard = ({ currentTurn, playerOneScore, playerTwoScore }) => {
   return (
     <header className="header">
-      <h2>Score Board</h2>
+      <h2>Score</h2>
       <div>
-        <h3>Player One: {playerOneScore} {currentTurn === 1 && "(Your Turn)"}</h3>
+        <h3>P1: {playerOneScore} {currentTurn === 1 && "(Your Turn)"}</h3>
       </div>
       <div>
-        <h3>Player Two: {playerTwoScore} {currentTurn === 2 && "(Your Turn)"}</h3>
+        <h3>P2: {playerTwoScore} {currentTurn === 2 && "(Your Turn)"}</h3>
       </div>
     </header>
   );
